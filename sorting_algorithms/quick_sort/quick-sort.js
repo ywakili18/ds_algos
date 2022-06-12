bfe1bfd1ff68ff6511c48ebfc9e70fa55cf70853
Yousof Wakili <ywakili18@gmail.com>
2022-06-11 18:41:51 -0700

    update

--- sorting_algorithms/quick_sort/quick-sort.js
+++ sorting_algorithms/quick_sort/quick-sort.js
@@ -14,25 +14,38 @@
 // and then swap the current element with the element at the pivot index
 // Swap the starting element (i.e the pivot) with the pivot index
 // return the pivot index
+
 function pivot(arr, start = 0, end = arr.length - 1) {
-  const swap = (arr, idx1, idx2) => {
+  // we need this pivot function, to return the index at where the pivot is located.
+  // anything the pivot (or middle point) is where:
+  // all values < pivot will be placed left of that pivot
+  // all values > pivot will be placed right of that pivot
+  function swap(arr, idx1, idx2) {
     return ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
   }
-  let swapIdx = start
+  // choose where the pivot starts first. it could be beginning, middle, or end of array.
   let pivot = arr[start]
-  // loop over everything after start
+  // we called swapIdx keeping track where the start index will be swapped with at the end of this function.
+  // remember, we need all values less to be left of pivot, all values right to be right of pivot.
+  let swapIdx = start
   for (let i = start + 1; i <= end; i++) {
     if (arr[i] < pivot) {
-      //
+      // increment to swapIdx, we need to return this later.
       swapIdx++
-      // start + 1 will increment 1 to the initial value. (i = 2 now)
+      // perform swap here, to push it left of the pivot.
       swap(arr, swapIdx, i)
     }
   }
-  // perform final swap, with original starting point (pivot) and index where it needs to be swapped at.
+  // we perform the final swap function again, because we will have the index at which the pivot (Start point)
+  // The  swapIdx will have been incremented as above to switch with the pivot (start point) of the array.
+  // As an example below: after performing the for loop above: we would have an array of [4,2,1,3,5,7,6,8]
+  // since the swapIdx has been incremented above, we know where to switch arr[pivot] with arr[swapIdx] at now!
+  // final array will now be [3, 2, 1, 4, 5, 7, 6, 8]
   swap(arr, start, swapIdx)
+  console.log(arr)
   return swapIdx
 }
+
 // psuedocode for quicksort
 // call the pivot helper on the array
 // when pivot is returned with the updated index, recursively call the pivot
@@ -53,6 +66,7 @@ const quickSort = (arr, left = 0, right = arr.length - 1) => {
     quickSort(arr, pivotIndex + 1, right)
   }
   // return the sorted array.
+  console.log(arr)
   return arr
 }
 quickSort([4, 2, 1, 7, 6])
